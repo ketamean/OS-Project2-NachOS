f95d960f937a0ab727fbbfb30e04c1ead2e49988
RokeMSE <rokeMSE123@gmail.com>
2024-03-25 23:41:15 +0700

    The first io function + User2System + System2User + Prototype +
    Added all the necessarities for testing the first funtions

---
+++ nachos-3.4/code/userprog/exception.cc
@@ -0,0 +1,158 @@
+// exception.cc 
+//	Entry point into the Nachos kernel from user programs.
+//	There are two kinds of things that can cause control to
+//	transfer back to here from user code:
+//
+//	syscall -- The user code explicitly requests to call a procedure
+//	in the Nachos kernel.  Right now, the only function we support is
+//	"Halt".
+//
+//	exceptions -- The user code does something that the CPU can't handle.
+//	For instance, accessing memory that doesn't exist, arithmetic errors,
+//	etc.  
+//
+//	Interrupts (which can also cause control to transfer from user
+//	code into the Nachos kernel) are handled elsewhere.
+//
+// For now, this only handles the Halt() system call.
+// Everything else core dumps.
+//
+// Copyright (c) 1992-1993 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#include "copyright.h"
+#include "system.h"
+#include "syscall.h"
+
+/* New file - call with: 'handle_SC_*functionname*' */
+#include "iokeyboard.cc"
+#include "iofile.cc"
+
+//----------------------------------------------------------------------
+// ExceptionHandler
+// 	Entry point into the Nachos kernel.  Called when a user program
+//	is executing, and either does a syscall, or generates an addressing
+//	or arithmetic exception.
+//
+// 	For system calls, the following is the calling convention:
+//
+// 	system call code -- r2
+//		arg1 -- r4
+//		arg2 -- r5
+//		arg3 -- r6
+//		arg4 -- r7
+//
+//	The result of the system call, if any, must be put back into r2. 
+//
+// And don't forget to increment the pc before returning. (Or else you'll
+// loop making the same system call forever!
+//
+//	"which" is the kind of exception.  The list of possible exceptions 
+//	are in machine.h.
+//----------------------------------------------------------------------
+
+
+/* USER SPACE AND KERNEL SPACE ALLOCATION */
+
+// Input: User(int) Space - buffer(int) limit
+// Output: A pointer to a character array (char*) containing the copied string.
+// Function: Copy a string of characters from user space to kernel space in NachOS. 
+char* User2System(int virtAddr, int limit) {
+	int idx;
+	int oneChar; // hold one char for itterating (duh)
+	char* kernelBuf = NULL; 
+	// Allocates memory for a character buffer (kernelBuf) in kernel space using the new operator. 
+	// The size of the buffer is limit + 1 to accommodate a terminal character ('\0').
+	kernelBuf = new char[limit + 1]; // Needed for terminal char*
+	if (kernelBuf == NULL)
+		return kernelBuf; // NULL => Non-successful allocation
+		
+	memset(kernelBuf, 0, limit + 1); 
+	// Initializes the allocated memory with zeros using the memset function. 
+	// This ensures that the memory buffer is properly initialized, especially for cases where the entire buffer may not be filled with data.
+	
+	for (idx = 0; idx < limit; idx++)
+	{
+		machine->ReadMem(virtAddr + idx, 1, &oneChar);
+		kernelBuf[idx] = (char)oneChar;
+		if (oneChar == 0)
+			break;
+	}
+	return kernelBuf; // NOTICE: This returns the kernelBuf POINTER, which holds the copied string from user space to kernel space.
+}
+
+// Input: User(int) Space - buffer(int) limit - buffer(char*) Space
+// Output: The number of characters actually copied. If successful, this should be equal to len. 
+// Function: Copy a string of characters from kernel space to user space in a NachOS. 
+int System2User(int virtAddr, int len, char* buffer) // NOTICE: This has added len parameter
+{
+	if (len < 0) return -1; 
+	if (len == 0)return len; // Your typical errors
+	int i = 0;
+	int oneChar = 0;
+	do{
+		oneChar = (int)buffer[i];
+		machine->WriteMem(virtAddr + i, 1, oneChar); 
+		// Writes the character oneChar to the memory location 
+		// specified by virtAddr + i in user space using the machine->WriteMem function.
+		i++;
+	} while (i < len && oneChar != 0);
+	return i;
+}
+
+
+/* EXCEPTION HANDLER */
+
+void ExceptionHandler(ExceptionType which)
+{
+    int type = machine->ReadRegister(2); // This is a global variable for SynchConsole
+    // NOTICE: R2 Contains the syscall task and the result(s)/variable(s) that will be return, the rest or just for variables handler
+
+
+switch (which) {
+	case NoException:
+		return;
+
+	case PageFaultException:
+		DEBUG('a', "\n No valid translation found");
+		printf("\n\n No valid translation found");
+		interrupt->Halt();
+		break;
+	case SysCallException:
+		switch(type) {
+
+			case SC_Halt:
+			// Input: None
+			// Output: System Shutdown Call
+			DEBUG('a', "\nShutdown, initiated by user program. ");
+			printf("\nShutdown, initiated by user program. ");
+			interrupt->Halt();
+			return;
+
+			case SC_ReadInt:
+		        	return handle_SC_ReadInt();
+			case SC_PrintInt:
+				return handle_SC_PrintInt();
+			case SC_ReadFloat:
+		        	return handle_SC_ReadFloat();
+			case SC_PrintFloat:
+		        	return handle_SC_PrintFloat();
+			case SC_ReadChar:
+		        	return handle_SC_ReadChar();
+			case SC_PrintChar:
+		        	return handle_SC_PrintChar();
+			case SC_ReadString:
+		        	return handle_SC_ReadString();
+			case SC_PrintString:
+		        	return handle_SC_PrintString();		
+
+
+    /* if ((which == SyscallException) && (type == SC_Halt)) {
+	DEBUG('a', "Shutdown, initiated by user program.\n");
+   	interrupt->Halt();
+    } else {
+	printf("Unexpected user mode exception %d %d\n", which, type);
+	ASSERT(FALSE);
+    } Old habit dies young*/
+}
---
+++ nachos-3.4/code/userprog/iokeyboard.cc
@@ -0,0 +1,73 @@
+void handle_SC_ReadInt() {
+	// Input: Keyboard
+	// Output: ONE interger
+	char* buffer;
+	int MAX_BUFFER = 255;
+	buffer = new char[MAX_BUFFER + 1];
+	int numbytes = gSynchConsole->Read(buffer, MAX_BUFFER);// Read the buffer char string and return it <= MAX_BUFFER (of course)
+	int number = 0; // Answer (if any)
+			
+	/* THIS IS WHERE THE MAGIC HAPPENS: TURNING CHAR* INTO INTERGER IF VALID */
+
+	// Set a bool for later (-) number handle                      
+	bool isNegative = false; 
+	int firstIndex = 0;
+	int lastIndex = 0;
+	if(buffer[0] == '-') // I think you know...
+	{
+		isNegative = true;
+		firstIndex = 1;
+		lastIndex = 1;                        			   		
+	}
+
+	// Checking if the char* is a valid interger
+	for(int i = firstIndex; i < numbytes; i++)					
+	{
+		if(buffer[i] == '.') /// Hypothetically: 125.0000000 is technically an interger 
+		{
+	    	int j = i + 1;
+	    	for(; j < numbytes; j++)
+	    	{
+			if(buffer[j] != '0') // Indicating this is a float
+			{
+		    	printf("\n\n The integer number is not valid");
+		    	DEBUG('a', "\n The integer number is not valid");
+		    	machine->WriteRegister(2, 0);
+		    	delete buffer;
+			interrupt->Halt();
+		    	return;
+			}
+	    	}
+	    	// If passes the test of .000000
+	    	lastIndex = i - 1;				
+	    	break;                           
+		}
+		else if(buffer[i] < '0' && buffer[i] > '9')
+		{
+	    	printf("\n\n The integer number is not valid");
+	    	DEBUG('a', "\n The integer number is not valid");
+	    	machine->WriteRegister(2, 0);
+	    	delete buffer;
+		interrupt->Halt();
+	    	return;
+		}
+		lastIndex = i;    
+	}			
+
+	// Valid interger, turn the char* buffer into numbers
+	for(int i = firstIndex; i<= lastIndex; i++)
+	{
+		number = number * 10 + (int)(buffer[i] - 48); 
+	}
+
+	// Make it (+) if is (-)
+	if(isNegative)
+	{
+		number = number * -1;
+	}
+	machine->WriteRegister(2, number); // Record the final number into r2 (result)
+	delete buffer; // For memory perserverance purpose
+	interrupt->Halt();
+	return;		
+}
+
