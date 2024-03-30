e62995ff1d4448620817c9c740b1d1d7f5f7b630
Anh Thu Be <blathu22@clc.fitus.edu.vn>
2024-03-30 17:00:33 +0700

    Complete quicksort (tested) + Update comment for ascii.c

--- nachos/nachos-3.4/code/test/ascii.c
+++ nachos/nachos-3.4/code/test/ascii.c
@@ -4,11 +4,13 @@ int main() {
     	OpenFileId file;
     	int i;
     	int j = 0;
-    	char table[170];
-    
+    	char table[256];
+    	// Tao file ascii.txt 
     	CreateF("ascii.txt");
+		// Mo file de tien hanh ghi
     	file = OpenF("ascii.txt", 0);
     	PrintString("\nBANG MA ASCII\n");
+		// Chay vong lap de in ra ky tu ascii
     	for (i = 32; i <= 127; i++)
     	{
         	PrintInt(i);
@@ -20,7 +22,8 @@ int main() {
         	table[j] = '\n';
         	j++;
     	}
-    	WriteF(table, 200, file);
+		// Tien hanh ghi vao file
+    	WriteF(table, 256, file);
     	CloseF(file);
     	Halt();
 }
